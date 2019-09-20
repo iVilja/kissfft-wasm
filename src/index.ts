8736e1108538cf22622242a27293914a7f8121a4
Vilja <i@vilja.me>
2019-09-20 19:18:38 +0000

    Export `wasm`.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { ComplexArray, ComplexNumber } from "./types"
 export { fft } from "./fft"
 export { ifft } from "./ifft"
-export { getWASMModule } from "./wasm"
+export { wasm } from "./wasm"
