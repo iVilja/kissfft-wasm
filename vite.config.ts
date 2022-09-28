a1c6b94a7c58a35e980ea828ac70ac25fbb896fb
Vilja <i@vilja.me>
2022-09-28 06:01:43 +0000

    Ignore EMSDK cache.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-
+    exclude: [
+      ...configDefaults.exclude,
+      "emsdk-cache"
+    ],
   },
 })
